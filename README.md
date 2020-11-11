c0c8b9adc471c5bf6925475c4d5073f33458138b
Patrick James Fontillas <me@pjfontillas.com>
2020-11-11 15:45:27 -0600

    Viable MVP.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# Memory Game
+• At the start of the game, the user is presented with a grid of 24 facedown cards.
+• Each card looks identical face down, but has a face-up value that is matched by only one
+other card on the table.
+• When the user clicks a card, it flips over revealing its value.
+• When the user clicks the next card, its value is revealed and then compared against the
+other face up card. If they are equal, both cards disappear. If they are different, they flip
+back down.
+• The game is continued until there are no cards left.
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
@@ -11,20 +21,3 @@ yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ js/memory.js
@@ -0,0 +1,205 @@
+import React, { Component } from 'react'
+import styles from '../styles/Home.module.css'
+import game from '../styles/game.module.css'
+
+class Card extends React.Component {
+	constructor(props) {
+		super(props);
+		this.flip = this.flip.bind(this);
+    this.hide = this.hide.bind(this);
+		this.state = {
+			active: false,
+		};
+	}
+
+	flip() {
+		const currentState = this.state.active;
+		if (this.props.isLocked() || this.state.hidden) return;
+
+		this.setState({ active: !currentState });
+		console.log('flipping card(' + this.props.value + ') state: ', !currentState);
+		if (!currentState) {
+			console.log('sending card value...', this.props.value);
+			this.props.onFlip(this.props.value, this.flip, this.hide);
+		} else {
+			console.log('sending null...');
+			this.props.onFlip(null, null);
+		}
+	}
+
+  hide() {
+    console.log('hiding card...');
+    this.setState({hidden:true});
+    setTimeout(() => {
+
+    }, 1500)
+  }
+
+  /**
+  CSS flip trick modified from good ol' David Walsh:
+  https://davidwalsh.name/demo/css-flip.php
+  */
+	render() {
+		return (
+			<div className={`${game.flipCard} animate__animated ${this.state.hidden ? `animate__rotateOut ${game.hidden}`: ''}`} onClick={this.flip}>
+				<div className={`${this.state.active ? game.flipCardInnerActive: game.flipCardInner }`}>
+					<div className={game.flipCardFront}></div>
+					<div className={game.flipCardBack}>
+						<p suppressHydrationWarning>{this.props.value}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+class Memory extends React.Component {
+	constructor(props) {
+		super(props);
+		this.checkForMatch = this.checkForMatch.bind(this);
+		this.isLocked = this.isLocked.bind(this);
+		this.state = {
+			first: {
+				value: null,
+				flip: null,
+        hide: null,
+			},
+			second: {
+				value: null,
+				flip: null,
+        hide: null,
+			},
+			locked: false,
+      cards: this.generateCards(),
+      matches: 0,
+		};
+	}
+
+	renderCard(value, key) {
+		return <Card value={value} key={key} onFlip={this.checkForMatch} isLocked={this.isLocked} />;
+	}
+
+	isLocked() {
+		return this.state.locked;
+	}
+
+	checkForMatch(cardValue, flip,hide) {
+		const first = this.state.first;
+    const second = this.state.second;
+
+		console.log('dumping first...', this.state.first);
+		console.log('dumping second...', this.state.second);
+
+		if (cardValue != null) {
+			if (first.value == null) {
+				console.log("Storing first value:", cardValue);
+				this.setState({
+					first: {
+						value: cardValue,
+						flip: flip,
+            hide: hide,
+					},
+				});
+			} else {
+				console.log("Storing second value:", cardValue);
+				this.setState({
+					second: {
+						value: cardValue,
+						flip: flip,
+            hide: hide,
+					},
+					locked: true,
+				}, () => {
+					// compare the values
+					console.log("Comparing values...");
+
+					setTimeout(() => {
+						this.setState({
+							locked: false,
+						}, () => {
+							if (this.state.first.value == this.state.second.value) {
+								console.log("It's a match!");
+                this.state.first.hide();
+                this.state.second.hide();
+                const matches = this.state.matches;
+                let _this = this;
+                this.setState({matches:matches+1}, () => {
+                  if (_this.state.matches == _this.props.pairs) {
+                    setTimeout(() => {
+                      alert("You win!");
+                    }, 1000);
+                  }
+                });
+							} else {
+								console.log("No match...");
+								this.state.first.flip();
+								this.state.second.flip();
+							}
+							this.setState({
+								first: {
+									value: null,
+									flip: null,
+									hide: null,
+								},
+								second: {
+									value: null,
+									flip: null,
+									hide: null,
+								},
+							});
+						});
+					}, 1000);
+				});
+			}
+		} else {
+      if (second.value != null) {
+        this.setState({
+          second: {
+            value: null,
+            flip: null,
+            hide: null,
+          },
+        });
+      } else {
+        this.setState({
+          first: {
+            value: null,
+            flip: null,
+            hide: null,
+          },
+        });
+      }
+    }
+	}
+
+  generateCards() {
+    const max = 13;
+    let cards = [];
+    for (let i = 1; i <= this.props.pairs; i++) {
+      let value = Math.floor(Math.random() * Math.floor(max)) + 1;
+      cards.push({
+        value: value,
+        key: Math.random(),
+      });
+      cards.push({
+        value: value,
+        key: Math.random(),
+      });
+    }
+    console.log('dumping cards...', cards);
+    return cards.sort(() => Math.random() - 0.5);
+  }
+
+	render() {
+    const _this = this;
+    return (
+        <div className={styles.grid}>
+          {this.state.cards.map((card) => {
+            return _this.renderCard(card.value, card.key)
+          })}
+        </div>
+    );
+	}
+}
+
+export default Memory
